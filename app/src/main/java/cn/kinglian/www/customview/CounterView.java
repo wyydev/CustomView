e533585b00f75f61af1ac1481b64cfdad4ff5582
ywy <wyyapply@163.com>
2018-06-28 14:28:51 +0800

    counterView 绘制完成

--- app/src/main/java/cn/kinglian/www/customview/CounterView.java
+++ app/src/main/java/cn/kinglian/www/customview/CounterView.java
@@ -1,9 +1,11 @@
 package cn.kinglian.www.customview;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -18,24 +20,33 @@ import android.view.View;
 
 public class CounterView extends View {
     private int defaultColor = Color.GRAY;
-    private Paint mTipsPaint;
-    private String mTips;
+    private Paint mTextPaint;
+    private String mTips = "可提现金额";
     private int mTipsColor = defaultColor;
-    private Paint mValuePaint;
-    private float maxValue = 100f;
-    private float mValue = 0;
+    private float maxValue = 1000f;
+    private float mValue = 855f;
+    private float mTextOffsetY = 30;
+    private float mTipsSize = 20;
+    private float mValueSize = 35;
 
+    private ValueAnimator.AnimatorUpdateListener updateListener;
+    private ValueAnimator animator;
+    private int duration = 1500;
     //半径
     private float mRadius = 300;
     //圆弧间的间距
     private float mSpace = 30;
+    /**
+     * 箭头与进度圆弧的距离
+     */
+    private float mArrowSpace = 20;
     //开始角度
     private int mStartAngle = 145;
     //划过角度
     private int mSweepAngle = 250;
     //指针每次旋转5度
-    private int mScaleAngle = 5;
-    private int mProgressColor = Color.YELLOW;
+    private int mScaleAngle = 2;
+    private int mProgressColor = Color.RED;
     private float mProgress = 0.5f;
     private Paint mArcPaint;
     private Paint mProgressArcPaint;
@@ -43,6 +54,8 @@ public class CounterView extends View {
     private float mInStrokeWidth = 4;
     private float mCenterX;
     private float mCenterY;
+    private boolean isShowTips = true;
+    private boolean isShowValue = true;
 
     public CounterView(Context context) {
         this(context, null);
@@ -66,6 +79,26 @@ public class CounterView extends View {
         mProgressArcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mProgressArcPaint.setStyle(Paint.Style.STROKE);
         mProgressArcPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        updateListener = animation -> {
+            mValue = maxValue * (float) animation.getAnimatedValue();
+            mProgress = (float) animation.getAnimatedValue();
+            invalidate();
+        };
+        animator = ValueAnimator.ofFloat(0, mValue / maxValue);
+        animator.addUpdateListener(updateListener);
+        animator.setDuration(duration);
+
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if ( animator != null){
+            animator.start();
+        }
     }
 
     @Override
@@ -124,15 +157,15 @@ public class CounterView extends View {
         inRectF = new RectF(centerRectF.left + mSpace, centerRectF.top + mSpace,
                 centerRectF.right - mSpace, centerRectF.bottom - mSpace);
 
-
         Log.e("CounterView", "width：" + w + "   height:" + h);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         drawArc(canvas);
-    }
 
+        drawText(canvas);
+    }
 
     private void drawArc(Canvas canvas) {
         mArcPaint.setStrokeWidth(mOutStrokeWidth);
@@ -147,17 +180,22 @@ public class CounterView extends View {
         //绘制指示器层点
         mArcPaint.setColor(defaultColor);
         mArcPaint.setStrokeWidth(mInStrokeWidth);
-        float progressAngle = (float) (2 * Math.PI / 360) * (mStartAngle + Math.round(mProgress * mSweepAngle / mScaleAngle) * mScaleAngle);
+        //将角度转为弧度 1度 = Math.PI / 180 弧度
+        float progressAngle = (float) (Math.PI / 180) * (mStartAngle
+                + Math.round(mProgress * mSweepAngle / mScaleAngle) * mScaleAngle);
         float radius = centerRectF.width() / 2;
         int size = mSweepAngle / mScaleAngle;
-        for (int i = 0; i < size; i++) {
-            float angle = (float) (2 * Math.PI / 360) * (mStartAngle + i * mScaleAngle);
-            Log.e("CounterView","angle:"+angle);
-            float angle2 = (float)(mStartAngle + i * mScaleAngle);
-            Log.e("CounterView","angle2:"+angle2);
-
+        for (int i = 0; i <= size; i++) {
+            float angle = (float) (Math.PI / 180) * (mStartAngle + i * mScaleAngle);
+            Log.e("CounterView", "angle:" + angle);
             float x = mCenterX + (float) (radius * Math.cos(angle));
             float y = mCenterY + (float) (radius * Math.sin(angle));
+
+            if(progressAngle>=angle){
+                mArcPaint.setColor(getProgressColor(mProgress));
+            }else{
+                mArcPaint.setColor(defaultColor);
+            }
             canvas.drawPoint(x, y, mArcPaint);
         }
 
@@ -167,15 +205,65 @@ public class CounterView extends View {
         float y = mCenterY + (float) (radius * Math.sin(progressAngle));
         canvas.drawPoint(x, y, mArcPaint);
 
-
         //绘制指示器
+        mArcPaint.setStrokeWidth(mInStrokeWidth / 3);
+        mArcPaint.setColor(mProgressColor);
+        //指示器的圆的半径
+        float indicatorRadius = mOutStrokeWidth;
+        //画进度指示器的圆
+        canvas.drawCircle(x, y, indicatorRadius, mArcPaint);
 
+        //箭头点的坐标
+        float x1 = mCenterX + (float) ((radius + mArrowSpace) * Math.cos(progressAngle));
+        float y1 = mCenterY + (float) ((radius + mArrowSpace) * Math.sin(progressAngle));
 
+        //算出指示器到指示器圆心的直线与指示器切线的夹角
+        float a = (float) Math.sin(radius / (radius + mArrowSpace));
+        //指示器与指示器圆的切线坐标角度
+        float angle2 = progressAngle + a;
+        //指示器与指示器圆的切线坐标1
+        float x2 = (float) (x + Math.cos(angle2) * indicatorRadius);
+        float y2 = (float) (y + Math.sin(angle2) * indicatorRadius);
+
+        //指示器与指示器圆的切线坐标角度
+        float angle3 = progressAngle - a;
+        //指示器与指示器圆的切线坐标2
+        float x3 = (float) (x + Math.cos(angle3) * indicatorRadius);
+        float y3 = (float) (y + Math.sin(angle3) * indicatorRadius);
+
+        Path path = new Path();
+        path.moveTo(x2, y2);
+        path.lineTo(x1, y1);
+        path.lineTo(x3, y3);
+        //画指示器的箭头
+        canvas.drawPath(path, mArcPaint);
         mArcPaint.setColor(defaultColor);
-        mArcPaint.setPathEffect(null);
+
         //画内层弧
         canvas.drawArc(inRectF, mStartAngle, mSweepAngle, false, mArcPaint);
 
+    }
+
+
+    private void drawText(Canvas canvas) {
+        //绘制tips
+        if (isShowTips && mTips != null) {
+            mTextPaint.setColor(mTipsColor);
+            mTextPaint.setTextAlign(Paint.Align.CENTER);
+            mTextPaint.setTextSize(mTipsSize);
+            float x = mCenterX;
+            float y = mCenterY - mTextOffsetY;
+            canvas.drawText(mTips, x, y, mTextPaint);
+        }
+
+        if (isShowValue) {
+            //绘制Value
+            mTextPaint.setColor(getProgressColor(mProgress));
+            mTextPaint.setTextSize(mValueSize);
+            float x1 = mCenterX;
+            float y1 = mCenterY + mTextOffsetY;
+            canvas.drawText(formatText(mValue, "￥%.2f"), x1, y1, mTextPaint);
+        }
     }
 
 
